92a8895aa00837c48d4e5f90d123a43e6b1dcf11
MoxPoto <41552639+MoxPoto@users.noreply.github.com>
2021-07-28 20:45:12 -0700

    df

--- jscontext_preload.js
+++ jscontext_preload.js
@@ -1,5 +1,4 @@
 // Creates class definitions and other things
-var global = new Function('return this;')()
 
 function Vector(x, y, z) {
 	this.x = x
@@ -11,4 +10,4 @@ Vector.prototype.toString = function() {
 	return "[" + this.x + ", " + this.y + ", " + this.z + "]"
 }
 
-global.Vector = Vector
+this.Vector = Vector
